2a3f6fcdbae9ca47fcb179bdec92b71beec6bb69
taoaustin <taoaustin@yahoo.ca>
2025-07-10 00:51:28 -0400

    try > 1$

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,7 @@ import {
 import { usePrivy, useWallets } from "@privy-io/react-auth";
 
 function App() {
-  // const [address, setAddress] = useState("");
   const { connectWallet } = usePrivy();
-  const { wallets } = useWallets();
   const [data, setData] = useState<FeeData | undefined>(undefined);
   const [overrideChain, setOverrideChain] = useState<number>(1);
 
@@ -62,62 +60,6 @@ function App() {
     fetchLiQuest();
   }, []);
 
-  async function claim(chainId: number) {
-    const wallet = wallets[0];
-    await wallet.switchChain(chainId);
-    const url = `https://li.quest/v1/integrators/nestwallet/withdraw/${chainId}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch: ${(await response.json()).message!}`);
-    }
-    const data = (await response.json()) as WithDrawResp;
-
-    const txnData = data.transactionRequest.data;
-    const prefix = "0xe5d64766";
-    let txnDataWithoutPrefix: string;
-    if (txnData.startsWith(prefix)) {
-      txnDataWithoutPrefix = txnData.slice(prefix.length);
-    } else {
-      txnDataWithoutPrefix = txnData;
-    }
-
-    var txnDataChunks = [];
-    for (let i = 0; i < txnDataWithoutPrefix.length; i += 64) {
-      txnDataChunks.push(txnDataWithoutPrefix.slice(i, i + 64));
-    }
-    console.log(txnDataChunks);
-
-    txnDataChunks = txnDataChunks.slice(0, 3);
-
-    const numTokens = (txnDataChunks.length - 2).toString(16);
-    if (txnDataChunks.length - 2 <= 0) {
-      console.log("NO TOKENS");
-      return;
-    }
-    // Pad numTokens with leading zeros until it is 64 characters long
-    const paddedNumTokens = numTokens.padStart(64, "0");
-    txnDataChunks[1] = paddedNumTokens;
-    var joinedChunks = "0xe5d64766" + txnDataChunks.join("");
-
-    const provider = await wallet.getEthereumProvider();
-
-    const chain = getChain(chainId);
-    const walletClient = createWalletClient({
-      account: wallet.address as Hex,
-      chain,
-      transport: custom(provider),
-    });
-    await walletClient.switchChain({
-      id: chainId,
-    });
-    const txHash = await walletClient.sendTransaction({
-      chain,
-      to: data.transactionRequest.to as Hex,
-      data: joinedChunks as Hex,
-    });
-    console.log(txHash);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
@@ -125,7 +67,7 @@ function App() {
           <p>loading...</p>
         ) : (
           <div className="flex flex-col gap-1">
-            <button
+            {/* <button
               className="bg-slate-400"
               onClick={() => {
                 claim(overrideChain);
@@ -133,8 +75,8 @@ function App() {
             >
               Click here for override ({getChain(overrideChain)?.name}
               )! (if chain doesn't show up)
-            </button>
-            <input
+            </button> */}
+            {/* <input
               type="number"
               step="1"
               min="0"
@@ -151,14 +93,9 @@ function App() {
                 // Save the input value to state
                 setOverrideChain(parseInt(e.target.value));
               }}
-            />
+            /> */}
             {data!.feeBalances.map((item: FeeBalance, idx: number) => (
-              <FeeDataItem
-                feeBalance={item}
-                onClick={() => {
-                  claim(item.chainId);
-                }}
-              />
+              <FeeDataItem feeBalance={item} />
             ))}
           </div>
         )}
@@ -167,9 +104,89 @@ function App() {
   );
 }
 
-function FeeDataItem(props: { feeBalance: FeeBalance; onClick: VoidFunction }) {
-  const { feeBalance, onClick } = props;
+function FeeDataItem(props: { feeBalance: FeeBalance }) {
+  const { feeBalance } = props;
+  const { wallets } = useWallets();
   const tokenBalances = feeBalance.tokenBalances;
+  const [enabled, setEnabled] = useState<boolean>(true);
+
+  async function claim(item: FeeBalance) {
+    const wallet = wallets[0];
+    await wallet.switchChain(item.chainId);
+    const url = `https://li.quest/v1/integrators/nestwallet/withdraw/${item.chainId}`;
+    const response = await fetch(url);
+    // if (!response.ok) {
+    //   throw new Error(`Failed to fetch: ${(await response.json()).message!}`);
+    // }
+    const resp = (await response.json()) as WithDrawResp;
+
+    // const txnData = resp.transactionRequest.to;
+
+    const prefix0 = "0xe5d64766";
+    const prefix1 =
+      "0000000000000000000000000000000000000000000000000000000000000020";
+    // let txnDataWithoutPrefix: string;
+    // if (txnData.startsWith(prefix)) {
+    //   txnDataWithoutPrefix = txnData.slice(prefix.length);
+    // } else {
+    //   txnDataWithoutPrefix = txnData;
+    // }
+
+    var tokens: string[] = [];
+    for (let i = 0; i < item.tokenBalances.length; i++) {
+      if (tokens.length >= 38) {
+        break;
+      }
+      let tokenBalance = item.tokenBalances[i];
+      let token = item.tokenBalances[i].token;
+      if (parseFloat(tokenBalance.amountUsd) < 1) {
+        continue;
+      }
+      console.log(tokenBalance.amountUsd);
+      tokens.push(token.address.substring(2).padStart(64, "0"));
+    }
+    if (tokens.length == 0) {
+      console.log("no tokens to withdraw");
+      return;
+    }
+
+    // var txnDataChunks = [];
+    // for (let i = 0; i < txnDataWithoutPrefix.length; i += 64) {
+    //   txnDataChunks.push(txnDataWithoutPrefix.slice(i, i + 64));
+    // }
+    // console.log(txnDataChunks);
+
+    // txnDataChunks = txnDataChunks.slice(0, 3);
+
+    const numTokens = tokens.length.toString(16);
+    // if (txnDataChunks.length - 2 <= 0) {
+    //   console.log("NO TOKENS");
+    //   return;
+    // }
+    // Pad numTokens with leading zeros until it is 64 characters long
+    const paddedNumTokens = numTokens.padStart(64, "0");
+    // txnDataChunks[1] = paddedNumTokens;
+    // var joinedChunks = "0xe5d64766" + txnDataChunks.join("");
+
+    const data = prefix0 + prefix1 + paddedNumTokens + tokens.join("");
+    const provider = await wallet.getEthereumProvider();
+
+    const chain = getChain(item.chainId);
+    const walletClient = createWalletClient({
+      account: wallet.address as Hex,
+      chain,
+      transport: custom(provider),
+    });
+    await walletClient.switchChain({
+      id: item.chainId,
+    });
+    const txHash = await walletClient.sendTransaction({
+      chain,
+      to: resp.transactionRequest.to as Hex,
+      data: data as Hex,
+    });
+    console.log(txHash);
+  }
 
   const usdAmount = tokenBalances
     .map((tokenBalance) => parseFloat(tokenBalance.amountUsd))
@@ -179,7 +196,17 @@ function FeeDataItem(props: { feeBalance: FeeBalance; onClick: VoidFunction }) {
   return (
     <button
       className="flex flex-col rounded-md bg-slate-700 p-3"
-      onClick={onClick}
+      onClick={async () => {
+        setEnabled(false);
+        try {
+          await claim(feeBalance);
+        } catch (e) {
+          console.log("THERE WAS AN ERROR", e);
+        } finally {
+          setEnabled(true);
+        }
+      }}
+      disabled={!enabled}
     >
       <span className="flex-1 text-[16px]">
         Chain: {chain ? chain.name : feeBalance.chainId}
